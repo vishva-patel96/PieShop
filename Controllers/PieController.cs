a122fc7f9307d7fa342ed0f43bb59831d7e56694
Vishva <1996vishva@gmail.com>
2025-06-15 14:48:27 -0400

    added view and controller

---
+++ Controllers/PieController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PieShop.Models;
+
+namespace PieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IPieRepository _pieRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+            _pieRepository = pieRepository;
+        }
+        public IActionResult List()
+        {
+            //ViewBag.CurrentCategory = "Cheese cakes";
+            return View(_pieRepository.AllPies);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
+using PieShop.Models;
 using Microsoft.AspNetCore.Builder;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+builder.Services.AddScoped<IPieRepository, MockPieRepository>();
 var app = builder.Build();
 
 app.UseStaticFiles();
