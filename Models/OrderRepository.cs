cc294bfa6fa5d024c9453c746f40308fbe14205d
vishva <1996vishva@gmail.com>
2025-06-30 22:16:12 -0400

    added new table in database,

---
+++ Models/OrderRepository.cs
@@ -0,0 +1,41 @@
+﻿namespace PieShop.Models
+{
+    public class OrderRepository :IOrderRepository
+    {
+        private readonly PieShopDbContext _bethanysPieShopDbContext;
+        private readonly IShoppingCart _shoppingCart;
+
+        public OrderRepository(PieShopDbContext bethanysPieShopDbContext, IShoppingCart shoppingCart)
+        {
+            _bethanysPieShopDbContext = bethanysPieShopDbContext;
+            _shoppingCart = shoppingCart;
+        }
+
+        public void CreateOrder(Order order)
+        {
+            order.OrderPlaced = DateTime.Now;
+
+            List<ShoppingCartItem>? shoppingCartItems = _shoppingCart.ShoppingCartItems;
+            order.OrderTotal = _shoppingCart.GetShoppingCartTotal();
+
+            order.OrderDetails = new List<OrderDetail>();
+
+            foreach (ShoppingCartItem? shoppingCartItem in shoppingCartItems)
+            {
+                var orderDetail = new OrderDetail
+                {
+                    Amount = shoppingCartItem.Amount,
+                    PieId = shoppingCartItem.Pie.PieId,
+                    Price = shoppingCartItem.Pie.Price
+                };
+
+                order.OrderDetails.Add(orderDetail);
+            }
+
+            _bethanysPieShopDbContext.Orders.Add(order);
+
+            _bethanysPieShopDbContext.SaveChanges();
+        }
+
+    }
+}
