65a348a152e7e4756ec31e73ae7c3a8b346b7353
vishva <1996vishva@gmail.com>
2025-06-30 21:19:38 -0400

    added shopping cart

--- Controllers/PieController.cs
+++ Controllers/PieController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PieShop.Models;
+using PieShop.ViewModels;
 
 namespace PieShop.Controllers
 {
@@ -27,5 +28,25 @@ namespace PieShop.Controllers
             }
             return View(pie);
         }
+        //public ViewResult List(string category)
+        //{
+        //    IEnumerable<Pie> pies;
+        //    string? currentCategory;
+
+        //    if (string.IsNullOrEmpty(category))
+        //    {
+        //        pies = _pieRepository.AllPies.OrderBy(p => p.PieId);
+        //        currentCategory = "All pies";
+        //    }
+        //    else
+        //    {
+        //        pies = _pieRepository.AllPies.Where(p => p.Category.CategoryName == category)
+        //            .OrderBy(p => p.PieId);
+        //        currentCategory = _categoryRepository.AllCategories.FirstOrDefault(c => c.CategoryName == category)?.CategoryName;
+        //    }
+
+        //    return View(new PieListViewModel(pies, currentCategory));
+        //}
+
     }
 }
